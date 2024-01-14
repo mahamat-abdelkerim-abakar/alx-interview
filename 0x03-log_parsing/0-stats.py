287451013350a5fd4ceeab3ce1310ceeb1ebc736
Mahamat Abdelkerim Abakar <65360822+mahamat-abdelkerim-abakar@users.noreply.github.com>
2024-01-14 23:24:39 +0530

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,39 +1,71 @@
 #!/usr/bin/python3
 """
-Log parsing
-"""
+Write a script that reads stdin line by line and computes metrics:
 
+Input format:
+
+<IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
+
+(if the format is not this one, the line must be skipped)
+
+After every 10 lines and/or a keyboard interruption (CTRL + C),
+print these statistics from the beginning:
+    - Total file size: File size: <total size>
+    - where <total size> is the sum of all previous <file size>
+      (see input format above)
+
+Number of lines by status code:
+    - possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
+    - if a status code doesn’t appear or is not an integer,
+      don’t print anything for this status code
+    - format: <status code>: <number>
+status codes should be printed in ascending order
+"""
 import sys
 
+
+badge = 0
+total_size = 0
+code_dict = {200: 0, 301: 0, 400: 0, 401: 0,
+             403: 0, 404: 0, 405: 0, 500: 0}
+
+
+def print_badge():
+    print("File size: {}".format(total_size))
+    keys = list(code_dict.keys())
+    keys.sort()
+    for key in keys:
+        if code_dict[key] > 0:
+            print("{}: {}".format(key, code_dict[key]))
+
+
 if __name__ == '__main__':
-
-    filesize, count = 0, 0
-    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
-    stats = {k: 0 for k in codes}
-
-    def print_stats(stats: dict, file_size: int) -> None:
-        print("File size: {:d}".format(filesize))
-        for k, v in sorted(stats.items()):
-            if v:
-                print("{}: {}".format(k, v))
-
     try:
         for line in sys.stdin:
-            count += 1
-            data = line.split()
+            words = line.rstrip().split()
+            if (len(words) != 9) or ('"GET' not in words) \
+                    or ('HTTP/1.1"' not in words):
+                continue
             try:
-                status_code = data[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except BaseException:
-                pass
-            try:
-                filesize += int(data[-1])
-            except BaseException:
-                pass
-            if count % 10 == 0:
-                print_stats(stats, filesize)
-        print_stats(stats, filesize)
+                code = int(words[-2])
+            except ValueError:
+                continue
+
+            if code in code_dict:
+                try:
+                    total_size += int(words[-1])
+                except ValueError:
+                    continue
+                code_dict[code] += 1
+            else:
+                continue
+            badge += 1
+
+            # Print stats after 10 badges
+            if not badge % 10:
+                print_badge()
+
     except KeyboardInterrupt:
-        print_stats(stats, filesize)
-        raise
+        # Print stats after keyboard interrupt, works.
+        print_badge()
+    print_badge()
